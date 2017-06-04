9c54b7e987da980a8429ab05e87ca4e2b2d6c85d
KUMA SYOGEI <NagaseYami@gmail.com>
2017-06-04 16:09:11 +0900

    ４キャラ追加 Damageバグ修正 スクリプトリメーク
    Signed-off-by: KUMA SYOGEI <NagaseYami@gmail.com>

--- Assets/Scripts/Game/GameManeger.cs
+++ Assets/Scripts/Game/GameManeger.cs
@@ -5,21 +5,51 @@ using UnityEngine.SceneManagement;
 
 public class GameManeger : MonoBehaviour
 {
-    public GameObject Player1, Player2;
-    public GameObject Player1WinText, Player2WinText;
+    GameObject Player1, Player2;
+    GameObject Player1WinText, Player2WinText;
+    GameObject Ball;
+    public int WinBallNum = 2;
+    public int BallRespawnTime = 10;
+    int BallRespawnTimer = 0;
 	public CursorControl.CHARATYPE CharaType1, CharaType2;	// キャラクタータイプ
 
 	// Use this for initialization
 	void Start()
 	{
-		// キャラクターのタイプを取得
-		CharaType1 = CursorControl.GetCharaType1();
+        Ball = GameObject.Find("Ball").gameObject;
+        if (Ball == null) { Debug.Log("Cant find Ball!"); }
+        Player1 = GameObject.Find("Player1").gameObject;
+        if(Player1 == null) { Debug.Log("Cant find Palyer1!"); }
+        Player2 = GameObject.Find("Player2").gameObject;
+        if (Player2 == null) { Debug.Log("Cant find Player2!"); }
+
+        Player1WinText = Player1.transform.Find("Canvas").gameObject.transform.Find("WinText").gameObject;
+        if (Player1WinText == null) { Debug.Log("Cant find Player1WinText!"); }
+        Player2WinText = Player2.transform.Find("Canvas").gameObject.transform.Find("WinText").gameObject;
+        if (Player2WinText == null) { Debug.Log("Cant find Player2WinText!"); }
+
+        // キャラクターのタイプを取得
+        CharaType1 = CursorControl.GetCharaType1();
 		CharaType2 = CursorControl.GetCharaType2();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+        if (!Ball.activeSelf)
+        {
+            BallRespawnTimer++;
+        }
+        else
+        {
+            BallRespawnTimer = 0;
+        }
+
+        if (BallRespawnTimer >= BallRespawnTime*60)
+        {
+            Ball.SetActive(true);
+        }
+
 		// キャラクタータイプ デバッグ用
 		if (Input.GetKeyDown(KeyCode.O))
 		{
@@ -39,11 +69,14 @@ public class GameManeger : MonoBehaviour
 			SceneManager.LoadScene("GameScene");
 		}
 
-        if (Player1.GetComponent<PlayerManager>().bDead)
+        if (Player1.GetComponent<PlayerManager>().bDead ||
+            Player2.GetComponent<PlayerManager>().BallCount >= WinBallNum
+            )
         {
             Player2WinText.SetActive(true);
         }
-		if (Player2.GetComponent<PlayerManager>().bDead)
+		if (Player2.GetComponent<PlayerManager>().bDead ||
+            Player1.GetComponent<PlayerManager>().BallCount >= WinBallNum)
 		{
 			Player1WinText.SetActive(true);
 		}
